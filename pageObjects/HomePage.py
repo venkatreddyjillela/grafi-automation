fc2392f9ef378e0ed480be409e66352710fb999a
venkatareddyjillela <venkatareddyjillela47842@gmail.com>
2023-05-25 14:50:44 +0530

    formatting

--- pageObjects/HomePage.py
+++ pageObjects/HomePage.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 class HomePage:
     # Home Page
     # button_first_draft_gen_xpath = '//h6[normalize-space()="First Draft Generator"]'
-    button_first_draft_gen_xpath = '//div[@class="MuiGrid-root MuiGrid-container MuiGrid-item MuiGrid-grid-xs-6 dashboardLeftContent css-sor6pw"]//div[1]//a[1]'
+    button_firstdraftGen_xpath = '//div[@class="MuiGrid-root MuiGrid-container MuiGrid-item MuiGrid-grid-xs-6 dashboardLeftContent css-sor6pw"]//div[1]//a[1]'
 
     # button_rephraser_xpath = '//h6[normalize-space()="Rephraser"]'
     button_rephraser_xpath = '//div[contains(@class,"MuiGrid-root MuiGrid-container MuiGrid-item MuiGrid-spacing-xs-4 css-h3jb2m")]//div[2]//a[1]'
@@ -18,7 +18,7 @@ class HomePage:
 
     def clickFirstDraftGen(self):
         self.driver.find_element(
-            By.XPATH, self.button_first_draft_gen_xpath).click()
+            By.XPATH, self.button_firstdraftGen_xpath).click()
 
     def clickRephraser(self):
         self.driver.find_element(By.XPATH, self.button_rephraser_xpath).click()
