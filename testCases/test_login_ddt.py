58e97917089386dde269fdb1b4618abea0ad8b4f
venkatareddyjillela <venkatareddyjillela47842@gmail.com>
2023-06-13 13:03:00 +0530

    login ddt

--- testCases/test_login_ddt.py
+++ testCases/test_login_ddt.py
@@ -2,6 +2,7 @@ import pytest
 from pageObjects.LoginPage import LoginPage
 from utilities.readProperties import ReadConfig
 from utilities.customLogger import LogGen
+from utilities.helperFunctions import HelperFunctions
 from utilities import XLUtils
 import time
 
@@ -9,46 +10,61 @@ import time
 class Test_002_DDT_Login():
     baseURL = ReadConfig.getApplicationURL()
     path = ".//TestData/LoginData.xlsx"
-    logger = LogGen.loggen()  # Logger
+    logger = LogGen.loggen() 
+    helper = HelperFunctions()
 
     @pytest.mark.regression
     def test_login_ddt(self, setup):
+        self.driver = setup
         self.logger.info("******* Starting Test_002_DDT_Login Test **********")
         self.logger.info("******* Starting Login DDT Test **********")
-        self.driver = setup
-        self.driver.get(self.baseURL)
-        self.driver.maximize_window()
-        self.lp = LoginPage(self.driver)
 
         self.rows = XLUtils.getRowCount(self.path, 'Sheet1')
-        self.logger.info('Number of rows...', self.rows)
+        self.logger.info('Number of rows = ' +  str(self.rows))
+        self.logger.info('Number of credentials login is verfied : ' +  str(self.rows-1))
         lst_status = []
 
-        for r in range(2, self.rows+1):
+        for r in range(2, self.rows):
             self.email = XLUtils.readData(self.path, 'Sheet1', r, 1)
             self.password = XLUtils.readData(self.path, 'Sheet1', r, 2)
             self.exp = XLUtils.readData(self.path, 'Sheet1', r, 3)
+            self.lp = self.helper.openLoginPage(setup)
+            # wait for login page to load
+            self.lp.waitForLoginPageToLoad()
 
-            self.lp.setEmail(self.email)
-            self.lp.setPassword(self.password)
+            if self.email:
+                self.lp.setEmail(self.email)
+            if self.password:
+                self.lp.setPassword(self.password)
             self.lp.clickLogin()
-            time.sleep(5)
+            try :
+                # wait for home page to load
+                self.lp.waitForHomePageToLoad()
+            except:
+                pass
 
             current_url = self.driver.current_url
             expected_url = self.baseURL+'home'
+
+            error_message = 'Invalid username or password.'
 
             if current_url == expected_url:
                 if self.exp == 'Pass':
                     self.logger.info("**** passed ****")
                     self.logger.info("**** Home Page is Opened ****")
-                    self.lp.clickSignoutDropdown()
-                    self.logger.info("**** Drop down is clicked ****")
-                    self.lp.clickSignout()
-                    self.logger.info("**** Click signout ****")
                     lst_status.append("Pass")
+
                 elif self.exp == 'Fail':
-                    self.logger.info("**** failed ****")
+                    self.logger.error("**** failed ****")
                     lst_status.append("Fail")
+
+                try :
+                    if self.lp.getErrorMessage() == error_message:
+                        self.logger.error("**** Error message is displayed for successful login ****")
+                        assert False
+                except :
+                    self.logger.info("**** Error message is not displayed ****")
+                    assert True
 
             elif current_url != expected_url:
                 if self.exp == 'Pass':
@@ -57,7 +73,16 @@ class Test_002_DDT_Login():
                 elif self.exp == 'Fail':
                     self.logger.info("**** passed ****")
                     lst_status.append("Pass")
+
+                if self.lp.getErrorMessage() == error_message:
+                    self.logger.info("**** Error message is displayed ****")
+                    assert True
+                else :
+                    self.logger.error("**** Error message is not displayed for login failure ****")
+                    assert False
+
             self.logger.info(lst_status)
+
         if "Fail" not in lst_status:
             self.logger.info("******* DDT Login test passed **********")
             self.driver.close()
@@ -70,3 +95,7 @@ class Test_002_DDT_Login():
         self.logger.info("******* End of Login DDT Test **********")
         self.logger.info(
             "**************** Completed  TC_LoginDDT_002 ************* ")
+        
+        self.logger.info("** Closing browser **")
+        # close browser 
+        self.driver.close()
